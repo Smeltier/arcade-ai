99e81308033b7166cfea5dd35ca93794e387898c
Smeltier <connect.goncalves@gmail.com>
2025-10-12 21:28:46 -0300

    att: atualiza as instancias das entidades

--- movimento_autonomo/main.py
+++ movimento_autonomo/main.py
@@ -1,11 +1,14 @@
 import pygame
 
 from world import World
-from atributes import Limits
+from atributes import Limits, WanderThresholds, BehaviorThresholds
 from moving_entity import MovingEntity
-
+from dynamic_states import Wander, Seek
+from input_controller import InputController
 
 def main():
+    pygame.init()
+
     WIDTH, HEIGHT = 800, 800
     SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
     CLOCK = pygame.time.Clock()
@@ -13,29 +16,46 @@ def main():
     world: World = World(SCREEN)
 
     # Definições da primeira entidade:
-    entity_one_limits = Limits(max_speed=200,
-                               max_acceleration=200,
-                               max_force=100)
+    entity_one_wander = WanderThresholds()
+    entity_one_behavior = BehaviorThresholds()
+    entity_one_limits = Limits(
+        max_speed=300,
+        max_acceleration=500,
+        max_force=1000
+    )
                                
-    entity_one = MovingEntity(WIDTH // 2, 
-                              HEIGHT // 2, 
-                              world, 
-                              limits=entity_one_limits)
+    entity_one = MovingEntity(
+        WIDTH // 2, 
+        HEIGHT // 2, 
+        world, 
+        limits=entity_one_limits,
+        wander_threshold=entity_one_wander,
+        behavior_threshold=entity_one_behavior
+    )
     
     # Definições da segunda entidade:
-    entity_two_limits = Limits(max_speed=200,
-                               max_acceleration=200,
-                               max_force=100)
+    entity_two_behavior = BehaviorThresholds()
+    entity_two_limits = Limits(
+        max_speed=300,
+        max_acceleration=500,
+        max_force=1000
+    )
                                
-    entity_two = MovingEntity(WIDTH // 4, 
-                              HEIGHT // 4, 
-                              world, 
-                              limits=entity_one_limits)
+    entity_two = MovingEntity(
+        WIDTH // 4, 
+        HEIGHT // 4, 
+        world, 
+        limits=entity_two_limits,
+        behavior_threshold=entity_two_behavior
+    )
     
     world.add_entity(entity_one)
     world.add_entity(entity_two)
 
-    pygame.init()
+    entity_one.state_machine.change_state(Wander(entity_one, entity_two))
+    entity_two.state_machine.change_state(Seek(entity_two, entity_one))
+
+    controller = InputController(entity_one, entity_two)
 
     running = True
     while running:
@@ -47,6 +67,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+            controller.handle_event(event)
 
         world.update(delta_time)
 
