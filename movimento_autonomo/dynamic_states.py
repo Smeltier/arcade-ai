df89b07397cbf430247aa137f4b3b811935f679b
Smeltier <connect.goncalves@gmail.com>
2025-10-13 19:52:52 -0300

    add: adiciona o PrioritySteering

--- movimento_autonomo/dynamic_states.py
+++ movimento_autonomo/dynamic_states.py
@@ -623,3 +623,32 @@ class BlendedSteering(State):
         self.character.change_color("white")
     
     def exit(self): pass
+
+class PrioritySteering (State):
+   def __init__(self, character, groups: list[BlendedSteering]) -> None:
+       super().__init__()
+
+       self.character = character
+       self.groups = groups
+       self.epsilon = 0.1
+
+   def execute(self):
+       steering = self.get_steering()
+       self.character.apply_steering(steering, self.character.delta_time)
+  
+   def get_steering(self) -> SteeringOutput:
+       steering = SteeringOutput()
+
+       for group in self.groups:
+           steering = group.get_steering()
+
+           if steering.linear.length() > self.epsilon or abs(steering.angular) > self.epsilon:
+               return steering
+      
+       return steering
+  
+   def enter(self):
+       print(f"[DEBUG] {self.character.ID} -> BlendedSteering")
+       self.character.change_color("white")
+  
+   def exit(self): pass
