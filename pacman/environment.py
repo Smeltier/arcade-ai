b6d49f1ea14deca868458730dbf415261e05215e
Smeltier <connect.goncalves@gmail.com>
2025-10-20 08:22:18 -0300

    test: muda o desenho do labirinto.

--- pacman/environment.py
+++ pacman/environment.py
@@ -6,7 +6,8 @@ class Environment:
         self.cell_size = cell_size
         
         self.maze = self.load_maze(maze_file)
-        self.maze_size = len(self.maze[0])
+        self.maze_rows = len(self.maze)
+        self.maze_cols = len(self.maze[0]) 
 
     def load_maze(self, file):
         maze = []
@@ -17,15 +18,31 @@ class Environment:
         return maze
 
     def draw(self):
-        SIZE = self.maze_size
+        ROWS = self.maze_rows
+        COLS = self.maze_cols
         CELL_SIZE = self.cell_size
         SCREEN = self.domain
+        RADIUS = CELL_SIZE // 2
 
-        for row in range(SIZE):
-            for col in range(SIZE):
-                x, y = col * CELL_SIZE, row * CELL_SIZE
+        for row in range(ROWS):
+            for col in range(COLS):
                 
-                if self.maze[row][col] == -1:
-                    pygame.draw.rect(SCREEN, "blue", (x, y, CELL_SIZE, CELL_SIZE))
-                if self.maze[row][col] ==  1:
-                    pygame.draw.circle(SCREEN, "white", (x + CELL_SIZE / 2, y + CELL_SIZE / 2), 2)
+                x_pos = col * CELL_SIZE
+                y_pos = row * CELL_SIZE
+
+                center_x = x_pos + RADIUS
+                center_y = y_pos + RADIUS
+
+                if self.maze[row][col] == 1:
+                    pygame.draw.circle(SCREEN, "white", (center_x, center_y), CELL_SIZE // 10)
+                    continue
+
+                pygame.draw.circle(SCREEN, "blue", (center_x, center_y), RADIUS)
+
+                if col + 1 < COLS and self.maze[row][col + 1] == -1:
+                    rect = pygame.Rect(center_x, y_pos, CELL_SIZE, CELL_SIZE)
+                    pygame.draw.rect(SCREEN, "blue", rect, width=4)
+                
+                if row + 1 < ROWS and self.maze[row + 1][col] == -1:
+                    rect = pygame.Rect(x_pos, center_y, CELL_SIZE, CELL_SIZE)
+                    pygame.draw.rect(SCREEN, "blue", rect, width=4)
