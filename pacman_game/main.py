819d42c8b1c09dcaf2e739befbdcd58dbf122392
Smeltier <connect.goncalves@gmail.com>
2025-11-03 10:42:15 -0300

    att: atualiza a instância do PacMan e do Ambiente.

--- pacman_game/main.py
+++ pacman_game/main.py
@@ -11,18 +11,18 @@ pygame.display.set_caption(game_title)
 
 FPS = 60
 WIDTH, HEIGHT = 900, 950
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 environment = Environment(
-    screen, 
-    'pacman_game/maze.txt'
+    screen = SCREEN, 
+    maze_file = 'pacman_game/maze.txt'
 )
 
 pacman = PacMan (
-    15 * environment.cell_width + environment.cell_width // 2, 
-    18 * environment.cell_height + environment.cell_height // 2, 
-    environment
+    x = 15 * environment.cell_width + environment.cell_width // 2, 
+    y = 18 * environment.cell_height + environment.cell_height // 2, 
+    environment = environment
 )
 
 environment.add_entity(pacman)
